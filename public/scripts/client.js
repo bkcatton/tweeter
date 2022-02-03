96ba4188bfbd8bf311bc0f7efe4e9140e46bbbcc
bkcatton <ben.k.catton@gmail.com>
2022-02-03 15:46:16 -0800

    added a sliding error message when characters exceed a certain limit.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,21 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function () {
-
+  //Setting up the container for all the tweets to be displayed
   const $allTweets = $('#container');
-
+//rendering all of the tweets by calling the create tweets function
   const renderTweets = function (tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
+
+    //emptying the container so tweets do not double up
+    const $errorMessageContainer = $('.errorMessageContainer');
+    $errorMessageContainer.empty();
+    $allTweets.empty();
+    
     for (let tweet of tweets) {
       const $anotherTweet = $createTweets(tweet);
       $allTweets.append($anotherTweet);
     }
   };
-
+//createTweets adds all of the html
   const $createTweets = (tweet) => {
     //creates a tweet based on data present
     const $likeRetweet = $('<output>').text('flag, retweet');
@@ -56,13 +61,14 @@ $(document).ready(function () {
   });
 
   
-
+  //Targeting the submit button for a tweet 
   const $submitButton = $('#submit-tweet'); 
 
   //on tweet button submit do this
   $submitButton.submit( function (event) {
     console.log("button worked", $(this).serialize());
     
+    //getting the character count
     const $inputField = $('#tweet-text');
     const inputValue = $inputField.val();
     const inputLength = inputValue.length;
@@ -70,18 +76,22 @@ $(document).ready(function () {
     event.preventDefault();
     const text = $(this).serialize();
 
+    //defining error messages etc for the character count
     if (inputLength <= 0 || inputLength > 140) {
-      const $errorMessage = $('<h5>').text("Error too many or not enough cvhars");
-      const $errorMessageInsert = $('<article>');
-      $errorMessageInsert.append($errorMessage);
-      const $errorMessageContainer = $('#container');
-      $errorMessageContainer.append($errorMessageInsert);
+      
+      const $errorMessageContainer = $('.errorMessageContainer');
+      const $errorMessage = $('<h5>').text("Error too many or not enough cvhars").addClass('errorMessage');
 
+      $errorMessageContainer.empty();
+      $errorMessageContainer.append($errorMessage).slideDown( 100 , function() {
+        // Animation complete.
+      });
 
       event.reset();
       return;
     }
-    
+   
+    //if tweet is in character range, the post method below will run
     $.ajax({
       url: 'http://localhost:8080/',
       method: 'POST',
@@ -95,6 +105,7 @@ $(document).ready(function () {
           dataType: 'json',
           success: (data) => {
             console.log('this request succeeded and here\'s the data', data);
+            
             renderTweets(data);
           },
           error: (error) => {
